7ac5ccb899f87e97c494f204bcdbca7396283122
unnati2007 <74820051+unnati2007@users.noreply.github.com>
2021-01-06 18:29:26 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,23 @@
+class Block {
+    constructor(x, y, width, height) {
+        var options = {
+          restitution :0.4,
+          friction :0.0,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.Visibility = 255;
+
+        World.add(world, this.body);
+      }
+      display() {
+       if(this.body.speed<3) {
+         push();
+         translate(this.body.position.x, this.body.position.y);
+         rectMode(CENTER);
+         rect(0,0,this.width,this.height);
+         pop();
+       } 
+    }
+}
